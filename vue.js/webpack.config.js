81eaa8509082010c506a819ea7907d62134a61cb
Joe Gregorio <joe@bitworking.org>
2018-02-05 21:12:17 -0500

    Consolidate the webpack.config.js files.

--- vue.js/webpack.config.js
+++ vue.js/webpack.config.js
@@ -1,39 +1,10 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
+const common = require('../webpack-common');
+common.plugins.push(
+  new CopyWebpackPlugin([
+    {from:'elements/images',to:'images'}
+  ])
+);
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js?[chunkhash]',
-    path: path.resolve(__dirname, 'dist')
-  },
-	module: {
-		rules: [
-			{
-        test: /\.css$/,
-        use:
-          ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: 'css-loader'
-          })
-      },
-		]
-	},
-	devServer: {
-    contentBase: './dist'
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new ExtractTextPlugin('styles.css?[contenthash]'),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    new CopyWebpackPlugin([
-      {from:'elements/images',to:'images'}
-    ]),
-  ]
-};
+module.exports = common;
